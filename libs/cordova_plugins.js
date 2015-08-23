db13908556f3a22a90ea7ed0cbb10b0fec269829
Superdragun <spade10@hotmail.com>
2015-08-23 12:06:24 -0500

    upd

--- libs/cordova_plugins.js
+++ libs/cordova_plugins.js
@@ -35,11 +35,68 @@ module.exports = [
         "file": "com.salesforce/com.salesforce.util.logger.js",
         "id": "com.salesforce.util.logger"
     }
+    {
+        "file": "org.apache.cordova.contacts/contacts.js",
+        "id": "org.apache.cordova.contacts.contacts",
+        "clobbers": [
+            "navigator.contacts"
+        ]
+    },
+    {
+        "file": "org.apache.cordova.contacts/Contact.js",
+        "id": "org.apache.cordova.contacts.Contact",
+        "clobbers": [
+            "Contact"
+        ]
+    },
+    {
+        "file": "org.apache.cordova.contacts/ContactAddress.js",
+        "id": "org.apache.cordova.contacts.ContactAddress",
+        "clobbers": [
+            "ContactAddress"
+        ]
+    },
+    {
+        "file": "org.apache.cordova.contacts/ContactError.js",
+        "id": "org.apache.cordova.contacts.ContactError",
+        "clobbers": [
+            "ContactError"
+        ]
+    },
+    {
+        "file": "org.apache.cordova.contacts/ContactField.js",
+        "id": "org.apache.cordova.contacts.ContactField",
+        "clobbers": [
+            "ContactField"
+        ]
+    },
+    {
+        "file": "org.apache.cordova.contacts/ContactFindOptions.js",
+        "id": "org.apache.cordova.contacts.ContactFindOptions",
+        "clobbers": [
+            "ContactFindOptions"
+        ]
+    },
+    {
+        "file": "org.apache.cordova.contacts/ContactName.js",
+        "id": "org.apache.cordova.contacts.ContactName",
+        "clobbers": [
+            "ContactName"
+        ]
+    },
+    {
+        "file": "org.apache.cordova.contacts/ContactOrganization.js",
+        "id": "org.apache.cordova.contacts.ContactOrganization",
+        "clobbers": [
+            "ContactOrganization"
+        ]
+    }   
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
 	"com.salesforce": "2.3.0",
+    "org.apache.cordova.contacts": "0.2.9",
     "cordova-plugin-whitelist": "1.0.0"
 }
 // BOTTOM OF METADATA
